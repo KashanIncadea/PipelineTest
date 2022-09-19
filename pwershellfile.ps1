a46c9ab5bc80c551f69285d6107ba2d5a6bded1c
KashanIncadea <112696123+KashanIncadea@users.noreply.github.com>
2022-09-19 03:03:54 +0200

    Update pwershellfile.ps1

--- pwershellfile.ps1
+++ pwershellfile.ps1
@@ -1,3 +1,3 @@
 az
 az account show
-az postgres server update --name kashanserver.postgres.database.azure.com --resource-group kashanresourcegroup --backup-retention 10
+az postgres server update --name kashanserver.postgres.database.azure.com --backup-retention 10
